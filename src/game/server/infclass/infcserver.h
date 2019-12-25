5bbed28bb9d8b8f8abb0e3f0ab52bdca38e1d4e5
Alexander Akulich <akulichalexander@gmail.com>
2019-12-25 21:09:39 +0300

    Introduce InfcServer

---
+++ src/game/server/infclass/infcserver.h
@@ -0,0 +1,24 @@
+#ifndef GAME_SERVER_INFCLASS_SERVER_H
+#define GAME_SERVER_INFCLASS_SERVER_H
+
+#include <engine/console.h>
+#include <engine/map.h>
+#include <engine/masterserver.h>
+#include <engine/server/register.h>
+#include <engine/shared/demo.h>
+#include <engine/shared/econ.h>
+#include <engine/shared/mapchecker.h>
+#include <engine/shared/netban.h>
+#include <engine/shared/network.h>
+#include <engine/shared/protocol.h>
+#include <engine/shared/snapshot.h>
+
+#include <engine/server/server.h>
+
+class CInfClassServer : public CServer
+{
+public:
+	CInfClassServer();
+};
+
+#endif // GAME_SERVER_INFCLASS_MAP_H
