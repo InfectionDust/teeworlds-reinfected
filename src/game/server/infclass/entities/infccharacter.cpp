5a16279af622371a5b02be05baa695190033b53e
Alexander Akulich <akulichalexander@gmail.com>
2020-12-30 01:34:49 +0300

    Implement jumping events for CInfClassPlayerClass

--- src/game/server/infclass/entities/infccharacter.cpp
+++ src/game/server/infclass/entities/infccharacter.cpp
@@ -1,14 +1,39 @@
 #include "infccharacter.h"
 
+#include "../classes/infcplayerclass.h"
+
 CInfClassCharacter::CInfClassCharacter(CGameWorld *pWorld)
 	: CCharacter(pWorld)
+	, m_Grounded(false)
 {
+}
+
+void CInfClassCharacter::Tick()
+{
+	CCharacter::Tick();
+	if (m_Core.m_TriggeredEvents & COREEVENTFLAG_AIR_JUMP) {
+		m_pClass->OnJumped(JumpType::Air);
+	} else if (m_Core.m_TriggeredEvents & COREEVENTFLAG_GROUND_JUMP) {
+		m_pClass->OnJumped(JumpType::Ground);
+	}
+	bool grounded = IsGrounded();
+	if (m_Grounded != grounded) {
+		if (grounded) {
+			m_pClass->OnGrounded();
+		}
+		m_Grounded = grounded;
+	}
 }
 
 void CInfClassCharacter::OnCharacterSpawn()
 {
 	// default health
 	IncreaseHealth(10);
+}
+
+void CInfClassCharacter::EnableJump()
+{
+	m_Core.m_Jumped &= ~2;
 }
 
 void CInfClassCharacter::SetActiveWeapon(int Weapon)
