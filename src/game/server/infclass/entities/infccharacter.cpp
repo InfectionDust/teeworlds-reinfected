5bf712808c46251ad457953298bafcdf3b48d5e6
Alexander Akulich <akulichalexander@gmail.com>
2020-12-30 01:23:46 +0300

    InfClassCharacter: Add initial API for weapons setup

--- src/game/server/infclass/entities/infccharacter.cpp
+++ src/game/server/infclass/entities/infccharacter.cpp
@@ -10,3 +10,21 @@ void CInfClassCharacter::OnCharacterSpawn()
 	// default health
 	IncreaseHealth(10);
 }
+
+void CInfClassCharacter::SetActiveWeapon(int Weapon)
+{
+	m_ActiveWeapon = Weapon;
+}
+
+void CInfClassCharacter::SetLastWeapon(int Weapon)
+{
+	m_LastWeapon = Weapon;
+}
+
+void CInfClassCharacter::TakeAllWeapons()
+{
+	for (WeaponStat &weapon : m_aWeapons)
+	{
+		weapon.m_Got = false;
+	}
+}
