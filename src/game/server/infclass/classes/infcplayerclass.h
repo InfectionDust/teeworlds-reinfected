2a4fdd78dee7b06eb58aef603d6c3101dc24403c
Alexander Akulich <akulichalexander@gmail.com>
2020-12-30 14:12:21 +0300

    Move weapon fire processing to CInfClassPlayerClass
    Omit Ninja for now.

--- src/game/server/infclass/classes/infcplayerclass.h
+++ src/game/server/infclass/classes/infcplayerclass.h
@@ -37,6 +37,14 @@ public:
 	virtual void OnCharacterSpawned();
 	virtual void OnGrounded() { }
 	virtual void OnJumped(JumpType jumpType);
+	virtual void OnWeaponFired(int Weapon);
+
+	virtual void OnHammerFired();
+	virtual void OnGunFired();
+	virtual void OnShotgunFired();
+	virtual void OnGrenadeFired();
+	virtual void OnLaserFired();
+	virtual void OnNinjaFired();
 
 protected:
 	explicit CInfClassPlayerClass();
