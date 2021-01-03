ae0fa60dd34155eba6afca491f7e60098f3d4e72
Alexander Akulich <akulichalexander@gmail.com>
2021-01-03 15:43:04 +0300

    Introduce CInfCEntity

---
+++ src/game/server/infclass/entities/infcentity.cpp
@@ -0,0 +1,15 @@
+#include "infcentity.h"
+
+#include <game/server/infclass/infcgamecontext.h>
+
+CInfCEntity::CInfCEntity(CInfClassGameContext *pGameContext, int ObjectType, vec2 Pos, int Owner)
+	: CEntity(pGameContext->GameWorld(), ObjectType, Pos)
+	, m_pContext(pGameContext)
+	, m_Owner(Owner)
+{
+}
+
+void CInfCEntity::Reset()
+{
+	GameServer()->m_World.DestroyEntity(this);
+}
---
+++ src/game/server/infclass/entities/infcentity.h
@@ -0,0 +1,25 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#ifndef GAME_SERVER_ENTITIES_INFC_ENTITY_H
+#define GAME_SERVER_ENTITIES_INFC_ENTITY_H
+
+#include <game/server/entity.h>
+
+class CInfClassGameContext;
+
+class CInfCEntity : public CEntity
+{
+public:
+	CInfCEntity(CInfClassGameContext *pGameContext, int ObjectType, vec2 Pos, int Owner);
+	CInfClassGameContext *GameContext() const { return m_pContext; }
+
+	int GetOwner() const { return m_Owner; }
+
+	void Reset() override;
+
+protected:
+	CInfClassGameContext *m_pContext = nullptr;
+	int m_Owner = 0;
+};
+
+#endif // GAME_SERVER_ENTITIES_INFC_ENTITY_H
